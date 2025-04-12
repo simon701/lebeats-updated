3b1ef871ebd7c4dc810c65b09017625af5f0abc5
Simon Maarawi <maarawisimon8@gmail.com>
2025-04-12 17:00:21 -0400

    Added LebEats logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import logo from '../assets/WhatsApp Image 2025-03-22 at 16.49.56_efc6d68a.jpg';
 
 const Header = () => (
-  <header style={{ padding: '1rem', background: '#222', color: '#fff' }}>
-    <h1>LebEats</h1>
+  <header style={{ display: 'flex', alignItems: 'center', gap:'1rem', padding: '1rem', background: '#222', color: 'fff'}}>
+    <img src={logo} alt="LebEats Logo" style={{ height: '50px', borderRadius: '8px' }} />
+    <h1 style={{ margin: 0 }}>LebEats</h1>
   </header>
 );
 
