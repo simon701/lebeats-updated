8e7bf64a3128a396f00ad43a5d765da0da937747
Simon Maarawi <maarawisimon8@gmail.com>
2025-04-12 17:12:21 -0400

    Added about page

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import { Routes, Route } from 'react-router-dom';
+import About from './pages/About';
 
 const App = () => {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
       <Home />
       <Footer />
     </>
---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About = () => (
+  <main style={{ padding: '2rem' }}>
+    <h2>About LebEats</h2>
+    <p>LebEats is your one-stop platform to discover and explore the best of Lebanon's food scene 🇱🇧</p>
+  </main>
+);
+
+export default About;
